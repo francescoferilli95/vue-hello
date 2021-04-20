1a456d892377a733d3d7958929d3df77f02a0115
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-04-20 14:58:52 +0200

    adding vue app && styles

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Welcome Dear User',
+        image: 'https://picsum.photos/1200/600',
+        imageText: 'Random Image',
+        message: 'This is your random image generator'
+    }
+});
